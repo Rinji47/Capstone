6448ac954ca69d32479bca5a2340207f82b0e4c0
Rinji47 <biswasankur06@gmail.com>
2026-02-21 13:06:25 +0545

    turned off debug

--- bloggers_haven/urls.py
+++ bloggers_haven/urls.py
@@ -2,13 +2,10 @@ from django.contrib import admin
 from django.urls import include, path, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-]
-
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

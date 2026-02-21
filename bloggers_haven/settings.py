119179463ada8515733b2347faee87680a06c36c
Rinji47 <biswasankur06@gmail.com>
2026-02-21 11:32:07 +0545

    removed cloudinary from installed apps

--- bloggers_haven/settings.py
+++ bloggers_haven/settings.py
@@ -125,16 +125,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-
 import os
 if DEBUG:
     STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field

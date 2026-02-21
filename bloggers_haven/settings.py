bc50f3dd353eec87c3e87991cd4cc88c5cac8e7c
Rinji47 <biswasankur06@gmail.com>
2026-02-21 11:20:21 +0545

    removed cloudinary from installed apps

--- bloggers_haven/settings.py
+++ bloggers_haven/settings.py
@@ -39,8 +39,6 @@ ALLOWED_HOSTS = ['*']  # Update this with your domain in production
 # Application definition
 
 INSTALLED_APPS = [
-    'cloudinary',
-    'cloudinary_storage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
